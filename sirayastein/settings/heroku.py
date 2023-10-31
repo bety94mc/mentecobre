fc9fe17faaa8a7dc89166263095871e1d8522733
bety94mc <bety94mc@gmail.com>
2023-10-31 23:03:10 +0100

    chore (sirayastein): try 'whitenoise.storage.CompressedStaticFilesStorage' to fix problem with whitenoise

--- sirayastein/settings/heroku.py
+++ sirayastein/settings/heroku.py
@@ -8,7 +8,7 @@ DATABASES = {'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )}
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 LOGGING = {
     'version': 1,
